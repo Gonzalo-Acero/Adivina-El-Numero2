d4e171230f325c1633e76aeb3bd8cfe7ffd35ee9
Gonzalo Julián Acero <gonzalo.acero@hotmail.com>
2025-01-11 16:57:01 -0300

    First Commit

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 Practicando y aprendiendo Python con ejercicios.
 Nivel `Beginner/Principiante`.
+
+
+
+#### Logica del programa
+
+**Intervalo inicial: [1, 10]**
+**Prediccion: 6**
+**Respuesta: "a"** `prediccion alta`
+**Intervalo: [1, 5]**
---
+++ adivina_el_numero2.py
@@ -0,0 +1,29 @@
+# Ahora el numero lo va a adivinar la computadora
+
+import random
+
+
+def advina_el_numero_PC(x):
+    
+    print("======================")
+    print(" Bienvenido al juego! ")
+    print("======================")
+    print(f"Selecciona un numero entre 1 y {x} para que la computadora intente adivinarlo...")
+    
+    limite_inferior = 1
+    limite_superior = x
+    
+    respuesta_usuario = ""
+    while respuesta_usuario != "c":
+        # Generar una prediccion
+        if limite_inferior != limite_superior:
+            prediccion = random.randint(limite_inferior, limite_superior)
+        else:
+            prediccion = limite_inferior # Tambien podria ser el limite superior
+            
+        # Obtener una respuesta del usuario
+        respuesta_usuario = input(f"Mi prediccion es: {prediccion}. Si es muy alta, ingresa (A). Si es muy baja, ingresa (B). Si es correcta, ingresa (C)").lower()
+        
+        if respuesta_usuario == "a":
+            limite_superior = prediccion - 1
+    
